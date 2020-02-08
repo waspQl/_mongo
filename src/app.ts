fb22a40232c3a94634ade56c9b96e2021a79c442
sakutaro <u390862@gmail.com>
2020-02-08 18:24:46 +0900

    update: admin dbに接続するように

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import mongodb from 'mongodb'
 const MongoClient = mongodb.MongoClient
 //import assert from 'assert'
 
-MongoClient.connect('mongodb://127.0.0.1:27017/sakutaro', (err, db) => {
+MongoClient.connect('mongodb://127.0.0.1:27017/admin', (err, db) => {
     //assert.equal(null, err)
     if (err === null) {
       console.log("Connected successfully to mongo server")
