ef1c67e787ad17a649f8eb33a1a81ed31c2d28f5
sakutaro <u390862@gmail.com>
2020-02-09 16:37:16 +0900

    update: コメント追加

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import mongodb from 'mongodb'
 const MongoClient = mongodb.MongoClient
 
+// .github/workflowsで定義した28001ポートを介し、
+// dockerで起動したmongodbのadminテーブルへ接続する
 MongoClient.connect('mongodb://127.0.0.1:28001/admin', (err, db) => {
     if (err === null) {
       console.log("Connected successfully to mongo server")
