6f1122be290d6eb18a29f7f436c11443cb6d381e
sakutaro <u390862@gmail.com>
2020-02-09 17:20:07 +0900

    update: localでも起動するように

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,23 @@
 import mongodb from 'mongodb'
 const MongoClient = mongodb.MongoClient
 
-// .github/workflowsで定義した28001ポートを介し、
-// dockerで起動したmongodbのadminテーブルへ接続する
-MongoClient.connect('mongodb://127.0.0.1:28001/admin', (err, db) => {
-    if (err === null) {
-      console.log("Connected successfully to mongo server")
-    } else {
-      console.log("Connection failure to mongo server", err)
-    }
+// 引数が渡ってきた場合はそれをport番号として使う
+let port = 27017
+if (
+  process.argv[2]
+  && Number.isInteger(
+    parseInt(process.argv[2])
+  )
+) port = parseInt(process.argv[2])
+// mongoへの接続確認用collection
+const collection = 'admin'
 
-    db.close()
+MongoClient.connect('mongodb://127.0.0.1:' + port + '/' + collection, (err, db) => {
+  if (err === null) {
+    console.log("Connected successfully to mongo server")
+  } else {
+    console.log("Connection failure to mongo server", err)
+  }
+
+  db.close()
 })
